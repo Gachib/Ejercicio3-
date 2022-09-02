a3de1d410f7fdcd18a395af91bf9c141a77e5194
Gachib <brayan.aruquipa00@gmail.com>
2022-09-01 21:03:36 -0300

    finish project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var resultado = suma(20,30,40);
+        System.out.println(resultado);
+
+        Coche miCoche = new Coche();
+        miCoche.maspuertas();
+        
+        System.out.println(miCoche.puertas);
+    }
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+class Coche{
+    public int puertas = 4;
+
+    public void maspuertas(){
+        this.puertas++;
     }
 }
